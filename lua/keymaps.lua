bc47579c115ce3319b317a816f1101c2b780105b
isaacgale <138847930+isaacgale@users.noreply.github.com>
2024-11-22 17:09:14 +1100

    added work stuff

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,6 +29,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -39,5 +39,6 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+vim.o.scrolloff = 10
 
 -- vim: ts=2 sts=2 sw=2 et
